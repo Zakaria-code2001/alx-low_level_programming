45ed1013ad80b45688a69ceda1b983f4c4c9da2c
Zakaria Mohammadi <zakariamohammadi91@gmail.com>
2023-06-09 08:39:48 -0700

    end

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,26 +1,29 @@
 #include "main.h"
+
 /**
- * _atoi - Convert  String Into  Integer.
+ * _atoi - Convert  string to  integer.
  *
- * @s: The string to use.
+ * @s: the string to use.
  *
  * Return: int.
  */
+
 int _atoi(char *s)
 {
- 	int sign = 1, i = 0;
- 	unsigned int res = 0;
-        while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
- 	{
- 		if (s[i] == '-')
- 			sign *= -1;
- 		i++;
- 	}
- 	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
- 	{
- 		res = (res * 10) + (s[i] - '0');
- 		i++;
- 	}
- 	res *= sign;
-  return (res);
- }
+	int sign = 1, i = 0;
+	unsigned int res = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= sign;
+	return (res);
+}
