dc6d2df5b4c7d800fb7c83ef7f310abc7fc4df54
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2023-07-04 15:17:45 -0700

    finish

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a LL
+ * @h: points to the 1st Node in the LL
+ *
+ * Return: number of elements in list free
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t length = 0;
+	int diff;
+	listint_t *temp;
+
+	if (!h || !*h)
+		return (0);
+
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			length++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			length++;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (length);
+}
