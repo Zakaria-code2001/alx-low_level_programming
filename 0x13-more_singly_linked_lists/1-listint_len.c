540f691c2992874dc9dea6f8f72eb617dda1a180
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2023-07-04 12:14:04 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - this cmd returns number of elements in linked lists
+ * @h: the linked list is of type listint_t 
+ *
+ * Return: n° of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t number = 0;
+
+	while (h)
+	{
+		number++;
+		h = h->next;
+	}
+
+	return (number);
+}
