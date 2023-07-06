44198578e38ad71481a850bf99f3349d86173bd7
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2023-07-06 06:00:47 -0700

    task2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - it print the binary of  decimal num
+ * @n: num to be printed in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int j, check = 0;
+	unsigned long int currently;
+
+	for (j = 63; j >= 0; j--)
+	{
+		currently = n >> i;
+
+		if (currently & 1)
+		{
+			_putchar('1');
+			check++;
+		}
+		else if (check)
+			_putchar('0');
+	}
+	if (!check)
+		_putchar('0');
+}
